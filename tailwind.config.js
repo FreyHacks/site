a24ffd7df2292aac802f4419994002dbe4432a45
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-01-30 15:19:58 +0530

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode:"jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkmode: true,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
