cf270689ba523e9e631a16c69e2c283705c6650b
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-02-03 15:41:32 +0530

    edited package.json and some boilerplate

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
           Welcome to abra ka dabra <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
