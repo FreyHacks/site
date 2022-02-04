1b754e5e5cded27f993bf50ec7d225e44d98b89d
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-02-04 17:38:38 +0530

    hero faq sponsers and svg

--- components/Main.jsx
+++ components/Main.jsx
@@ -3,12 +3,16 @@ import Footer from './Footer';
 import Info from './Info';
 import Events from './Events';
 import Sponsors from './Sponsors';
+import Faq from './Faq';
+import Hero from './Hero';
 const Main = () => {
   return( 
-      <div className='bg-transparent h-full w-full flex flex-col items-center pt-10'>
+      <div className='bg-transparent h-full w-full flex flex-col items-center pt-3 sm:pt-10'>
+        <Hero/>
         <Info/>
         <Events/>
         <Sponsors/>
+        <Faq/>
         <Footer/>
       </div>
   );
