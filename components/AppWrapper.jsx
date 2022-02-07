32f6b01e3d25ad1ea4ee1d4bb1421ed4d45d1600
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-02-07 16:57:59 +0530

    hero waves and sticky nav

--- components/AppWrapper.jsx
+++ components/AppWrapper.jsx
@@ -4,11 +4,9 @@ import Main from './Main.jsx';
 
 const AppWrapper = () => {
   return (
-      <div className='w-screen h-screen bg-gradient-to-b from-[#FFD749] via-white to-[#57ACED]  overflow-x-hidden overflow-y-scroll'>
-          <Nav />
-        <div className='subpixel-antialiased w-full'>
-          <Main/>
-        </div>
+      <div className='relative w-screen h-screen bg-gradient-to-b from-[#FFD749] via-white to-[#57ACED]  overflow-x-hidden overflow-y-auto'>
+        <Nav />
+        <Main/>
       </div>
   );
 };
