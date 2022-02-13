473a61809c404e7c4ec38ab09954bd67c1d28d4f
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-02-13 18:19:31 +0530

    meta bug

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -12,7 +12,7 @@ const Meta = () => {
         <meta property="og:url" content="https://freyhacks.tech/" />
         <meta property="og:image" content="https://freyhacks1.netlify.app/logo.png" />
         <meta property="og:description" content="A student-led Hackathon held this summer where students from around the world can build impactful projects and level up on their skills and win exciting prizes." />
-        <meta property="og:title" content="Frehacks" />
+        <meta property="og:title" content="Freyhacks" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>FreyHacks</title>
         <link rel="shortcut icon" href="./a.ico" />
