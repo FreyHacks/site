455de4f523ed792baca29e20f27f81e0496454e5
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-02-13 18:12:14 +0530

    icon

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -15,6 +15,7 @@ const Meta = () => {
         <meta property="og:title" content="Frehacks" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>FreyHacks</title>
+        <link rel="shortcut icon" href="./a.ico" />
       </Head>
   )
 }
