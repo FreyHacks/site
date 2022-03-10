10306f2fe2e78354fa210fb31f76143e3bd2141f
piyush <74035501+piyush-panpaliya@users.noreply.github.com>
2022-03-10 19:13:23 +0530

    Update Meta.jsx

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -11,20 +11,20 @@ const Meta = () => {
       />
       <meta
         property="og:description"
-        content="FreyHacks, named after the Norse God of Summer, is a summer hackathon designed to introduce students from diverse backgrounds to the hackathon culture. FreyHacks is for students of all skill levels, and strives to create an inclusive, collaborative and empowering environment for novices and experienced developers alike."
+        content="FreyHacks, named after the Norse God of Summer, is a summer hackathon designed to introduce students from diverse backgrounds to the hackathon culture. FreyHacks is for students of all skill levels, and strives to create an inclusive, collaborative and empowering environment for everyone."
       />
-      <meta name="keywords" content="hackathon, students, technology, mlh" />
-      <link rel="canonical" href="https://freyhacks.tech/" />
+      <meta name="keywords" content="hackathon, students, technology, mlh,Freyhacks ,frey ,hacks" />
+      <link rel="canonical" href="https://www.freyhacks.tech/" />
       <meta name="robots" content="all" />
       <meta property="og:site_name" content="FreyHacks" />
-      <meta property="og:url" content="https://freyhacks.tech/" />
+      <meta property="og:url" content="https://www.freyhacks.tech/" />
       <meta
         property="og:image"
-        content="https://freyhacks1.netlify.app/logo.png"
+        content="https://www.freyhacks1.netlify.app/logo.png"
       />
       <meta property="og:title" content="Freyhacks" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>FreyHacks</title>
+      <title>FreyHacks |Hackathon</title>
       <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       </React.Fragment>
   );
