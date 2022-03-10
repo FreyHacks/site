05dad161823d638bb9eb390850fbdbedecec1d63
piyush <74035501+piyush-panpaliya@users.noreply.github.com>
2022-03-10 20:50:37 +0530

    Update Meta.jsx

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -7,7 +7,7 @@ const Meta = () => {
       <meta charSet="UTF-8" />
       <meta
         name="description"
-        content="FreyHacks, named after the Norse God of Summer, is a summer hackathon designed to introduce students from diverse backgrounds to the hackathon culture. FreyHacks is for students of all skill levels, and strives to create an inclusive, collaborative and empowering environment for novices and experienced developers alike."
+        content="FreyHacks, named after the Norse God of Summer, is a summer hackathon designed to introduce students from diverse backgrounds to the hackathon culture. FreyHacks is for students of all skill levels, and strives to create an inclusive, collaborative and empowering environment for everyone."
       />
       <meta
         property="og:description"
@@ -20,11 +20,11 @@ const Meta = () => {
       <meta property="og:url" content="https://www.freyhacks.tech/" />
       <meta
         property="og:image"
-        content="https://www.freyhacks1.netlify.app/logo.png"
+        content="https://www.freyhacks.tech/logo.png"
       />
       <meta property="og:title" content="Freyhacks" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>FreyHacks |Hackathon</title>
+      <title>FreyHacks | Hackathon </title>
       <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       </React.Fragment>
   );
