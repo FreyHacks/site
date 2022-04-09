e58df7c5abc96a8bb6bcbf82d2ed9fccb9244ebe
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-04-09 10:45:21 +0530

    cleanup

--- components/AppWrapper.jsx
+++ components/AppWrapper.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
 import Nav from './Nav.jsx';
 import Main from './Main.jsx';
-// import { useState } from 'react';
-// import OnImagesLoaded from 'react-on-images-loaded';
 
 const AppWrapper = () => {
-  
   return (
-    // <OnImagesLoaded
-    //   onLoaded={() => setshow(true)}
-    //   onTimeout={() => setshow(true)}
-    //   timeout={1000}
-    // >
       <div  id='contain' className=' w-screen h-screen bg-gradient-to-b from-[#FFD749] via-white to-[#57ACED]  overflow-x-hidden overflow-y-scroll '>
-        <Nav external={false} />
+        <Nav  />
         <Main/>
       </div>
-    // </OnImagesLoaded>
   ); 
 };
 
