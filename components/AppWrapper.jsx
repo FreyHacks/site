230003241bae805cdb9078c2d5aa14ec58b68e5d
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-04-09 10:17:41 +0530

    merge conflict resolve

--- components/AppWrapper.jsx
+++ components/AppWrapper.jsx
@@ -5,22 +5,10 @@ import Main from './Main.jsx';
 // import OnImagesLoaded from 'react-on-images-loaded';
 
 const AppWrapper = () => {
-<<<<<<< HEAD
-  // const [show, setshow] = useState('home');  
-  return (
-    // <OnImagesLoaded
-    //   onLoaded={() => setshow(true)}
-    //   onTimeout={() => setshow(true)}
-    //   timeout={1000}
-    // >
-      <div  id='contain' className=' w-screen h-screen bg-gradient-to-b from-[#FFD749] via-white to-[#57ACED]  overflow-x-hidden overflow-y-scroll '>
-        <Nav r={false} />
-=======
   
   return (
       <div  id='contain' className=' w-screen h-screen bg-gradient-to-b from-[#FFD749] via-white to-[#57ACED]  overflow-x-hidden overflow-y-scroll'>
         <Nav external={false} />
->>>>>>> c1387b7 (renamed variables and props)
         <Main/>
       </div>
     // </OnImagesLoaded>
