9c544c5c79dd5a7c150eddf5ab0e7b6e40bd3dcf
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-04-09 10:37:52 +0530

    merge conflict

--- components/AppWrapper.jsx
+++ components/AppWrapper.jsx
@@ -3,17 +3,9 @@ import Nav from './Nav.jsx';
 import Main from './Main.jsx';
 
 const AppWrapper = () => {
-<<<<<<< HEAD
-  
-=======
->>>>>>> 4f949f2 (var edit)
   return (
       <div  id='contain' className=' w-screen h-screen bg-gradient-to-b from-[#FFD749] via-white to-[#57ACED]  overflow-x-hidden overflow-y-scroll '>
-<<<<<<< HEAD
-        <Nav external={false} />
-=======
         <Nav  />
->>>>>>> 4f949f2 (var edit)
         <Main/>
       </div>
   ); 
