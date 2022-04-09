259ba5d067c122eb28274753900f4c88f8968523
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-04-09 10:18:27 +0530

    merge conflict resolve

--- components/AppWrapper.jsx
+++ components/AppWrapper.jsx
@@ -7,7 +7,12 @@ import Main from './Main.jsx';
 const AppWrapper = () => {
   
   return (
-      <div  id='contain' className=' w-screen h-screen bg-gradient-to-b from-[#FFD749] via-white to-[#57ACED]  overflow-x-hidden overflow-y-scroll'>
+    // <OnImagesLoaded
+    //   onLoaded={() => setshow(true)}
+    //   onTimeout={() => setshow(true)}
+    //   timeout={1000}
+    // >
+      <div  id='contain' className=' w-screen h-screen bg-gradient-to-b from-[#FFD749] via-white to-[#57ACED]  overflow-x-hidden overflow-y-scroll '>
         <Nav external={false} />
         <Main/>
       </div>
